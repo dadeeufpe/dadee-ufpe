acca5d31e2290efcd8b876e9a31193e9e65def9e
Helena <m.helenalencar@gmail.com>
2020-07-11 04:00:40 -0300

    Funcionalidade do formulario de envio de mensagens para o Firebase.

--- assets/js/firebase/sendmessage.js
+++ assets/js/firebase/sendmessage.js
@@ -1,16 +1,60 @@
 // sendmessage.js
 // arquivo para gravação de dados da mensagem no firebase cloud firestore
-//
 // add record on 'inbox' collection
-db.collection("inbox").doc().set({
-    email:      "2-teste@teste.com",
-	message:    "2-Mensagem que foi escrita nesse campo",
-	name:       "2-Meu Nome Completo",
-	subject:    "2-Assunto da mensagem"
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-    console.error("Error adding document: ", error);
+//
+// coleta informações do formulário html
+function getInputsOnFormAndSave() {
+    var d               =   new Date();
+    var myTimestamp     =   ("00"+d.getFullYear()).slice(-4)+"-"+
+                            ("00"+String(parseInt(d.getMonth())+1)).slice(-2)+"-"+
+                            ("00"+d.getDate()).slice(-2)+" "+
+                            ("00"+d.getHours()).slice(-2)+":"+
+                            ("00"+d.getMinutes()).slice(-2)+":"+
+                            ("00"+d.getSeconds()).slice(-2)+"."+
+                            ("00"+d.getMilliseconds()).slice(-4);
+    var myIdDoc         =   ("00"+d.getFullYear()).slice(-4)+
+                            ("00"+String(parseInt(d.getMonth())+1)).slice(-2)+
+                            ("00"+d.getDate()).slice(-2)+
+                            ("00"+d.getHours()).slice(-2)+
+                            ("00"+d.getMinutes()).slice(-2)+
+                            ("00"+d.getSeconds()).slice(-2)+
+                            ("00"+d.getMilliseconds()).slice(-4)+
+                            Math.floor((Math.random() * 10000) + 1);
+    var dataEmail       =   document.getElementById('inputEmail'          ).value;
+    var dataMessage     =   document.getElementById('inputMessage'        ).value;
+    var dataName        =   document.getElementById('inputName'           ).value;
+    var dataSubject     =   document.getElementById('inputSubject'        ).value;
+    //
+    // Save 'new Reg'
+    // Grava o registro na coleção 'inbox' 
+    //
+    var newReg = db.collection("inbox").doc(myIdDoc);
+    newReg.set({
+        email:          dataEmail,     // email
+        message:        dataMessage,   // mensagem
+        name:           dataName,      // nome da pessoa que está enviando
+        subject:        dataSubject,   // assunto da mensagem
+        read:           false,         // 'lido' = true / não_lido = 'false'
+        status:         "pendente",    // pendente, realizado, cancelado
+        created_at:     myTimestamp,   // data de envio da mensagem (criação)
+        processed_at:   null,          // processada em 
+        deleted_at:     null           // deletada em
+    })
+    .then(function() {
+        alert("Mensagem enviada com sucesso!");
+        document.getElementById('formSendMessage').reset();
+    })
+    .catch(function(error) {
+        alert("Erro! Houve uma falha. A mensagem não foi enviada.");  
+        //console.error("Error adding document: ", error);
+    });  
+}
+//
+// evita o submit do formulário
+// chama função de salvamento no Firestore
+var form = document.getElementById('formSendMessage');
+form.addEventListener('submit', function(e) {
+    // impede o envio do form / 
+    e.preventDefault();
 });
+
