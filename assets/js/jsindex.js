8402d333ff50ef363ac8f743dee9ef2e77515d1f
Helena <m.helenalencar@gmail.com>
2020-07-12 23:05:01 -0300

    Página ainda em construção básica - melhorias na navegação dos links, index contem todas as seções do site.

--- assets/js/jsindex.js
+++ assets/js/jsindex.js
@@ -5,12 +5,86 @@
 //
 $(document).ready(function(){
   // -----------------------------------------------------------
+    // Status inicial da seção graduação
+  $("#sessao, #sessao1, #btn-ocultar, #btn-ocultar1, #btn-ocultar-fim, #btn-ocultar-fim1, .group-blog, .group-documentos").hide();
+  $("#comentario, #btn-exibir, #btn-exibir1, #comentario1").fadeIn();
+  
+  // -----------------------------------------------------------
   // ## HOME // PÁGINA INICIAL
   // scroll to 'home'
   // rola a tela para o topo da página
   $("#home").click(function()
   {
+    $(".group-index").fadeIn();
+    $(".group-blog").hide();
     $([document.documentElement, document.body]).animate({ scrollTop: $("body").offset().top - 70 }, 1000);
+    $("#navbar li a.active").removeClass("active"); //Remove any "active" class
+    $("#link-home").addClass("active"); //Add "active" class to selected tab
+  });
+  //
+  // -----------------------------------------------------------
+  // ## UNIVERSITY GRADUATE // GRADUAÇÃO
+  // scroll to 'university graduate'
+  // rola a tela para a seção 'graduação'
+  $("#graduacao").click(function()
+  {
+    $(".group-blog, .group-documentos, .group-eventos, .group-parceiros").hide();
+    $(".group-index").fadeIn();
+    $([document.documentElement, document.body]).animate({ scrollTop: $("#section-graduacao").offset().top - 70}, 1000);
+    $("#navbar li a.active").removeClass("active"); //Remove any "active" class
+    $("#link-graduacao").addClass("active"); //Add "active" class to selected tab
+  });
+  //
+  // -----------------------------------------------------------
+  // ## BLOG // BLOG
+  // scroll to 'contatos'
+  // rola a tela para a seção 'contatos'
+  $("#blog").click(function()
+  {
+    $(".group-index, .group-documentos, .group-eventos, .group-parceiros").hide();
+    $(".group-blog").fadeIn();
+    $([document.documentElement, document.body]).animate({ scrollTop: $("#sectionblog").offset().top - 70}, 1000);
+    $("#navbar li a.active").removeClass("active"); //Remove any "active" class
+    $("#link-blog").addClass("active"); //Add "active" class to selected tab
+  });
+  //
+    // -----------------------------------------------------------
+  // ## PARTNER // PARCEIROS
+  // scroll to 'partner'
+  // rola a tela para a seção 'parceiros'
+  $("#parceiros").click(function()
+  {
+    $(".group-index, group-documentos, .group-eventos, .group-blog").hide();
+    $(".group-parceiros").fadeIn();
+    $([document.documentElement, document.body]).animate({ scrollTop: $("#section-parceiros").offset().top - 70}, 1000);
+    $("#navbar li a.active").removeClass("active"); //Remove any "active" class
+    $("#link-parceiros").addClass("active"); //Add "active" class to selected tab
+  });
+  //
+  // -----------------------------------------------------------
+  // ## DOCUMENTS // DOCUMENTOS
+  // scroll to 'documents'
+  // rola a tela para a seção 'documentos'
+  $("#documentos").click(function()
+  {
+    $(".group-index, .group-blog, .group-eventos, .group-parceiros").hide();
+    $(".group-documentos").fadeIn();
+    $([document.documentElement, document.body]).animate({ scrollTop: $("#section-documentos").offset().top - 70}, 1000);
+    $("#navbar li a.active").removeClass("active"); //Remove any "active" class
+    $("#link-documentos").addClass("active"); //Add "active" class to selected tab
+  });
+  //
+   // -----------------------------------------------------------
+  // ## EVENTS // EVENTOS
+  // scroll to 'events'
+  // rola a tela para a seção 'eventos'
+  $("#eventos").click(function()
+  {
+    $(".group-index, .group-blog, .group-documentos, .group-parceiros").hide();
+    $(".group-eventos").fadeIn();
+    $([document.documentElement, document.body]).animate({ scrollTop: $("#section-eventos").offset().top - 70}, 1000);
+    $("#navbar li a.active").removeClass("active"); //Remove any "active" class
+    $("#link-eventos").addClass("active"); //Add "active" class to selected tab
   });
   //
   // -----------------------------------------------------------
@@ -19,7 +93,11 @@ $(document).ready(function(){
   // rola a tela para a seção 'contatos'
   $("#contatos").click(function()
   {
+    $(".group-blog, .group-eventos, .group-parceiros, .group-documentos").hide();
+    $(".group-index").fadeIn();
     $([document.documentElement, document.body]).animate({ scrollTop: $("#section3").offset().top -70 }, 1000);
+    $("#navbar li a.active").removeClass("active"); //Remove any "active" class
+    $("#link-contatos").addClass("active"); //Add "active" class to selected tab
   });
   //
   // -----------------------------------------------------------
@@ -28,23 +106,15 @@ $(document).ready(function(){
   // rola a tela para a seção 'sobre'
   $("#sobre").click(function()
   {
-    $([document.documentElement, document.body]).animate({ scrollTop: $("#section2").offset().top -70 }, 1000);
+    $(".group-blog, .group-eventos, .group-parceiros, .group-documentos").hide();
+    $(".group-index").fadeIn();
+    $([document.documentElement, document.body]).animate({ scrollTop: $("#section-sobre").offset().top -70 }, 1000);
+    $("#navbar li a.active").removeClass("active"); //Remove any "active" class
+    $("#link-sobre").addClass("active"); //Add "active" class to selected tab
   });
   // -----------------------------------------------------------
   // ## GRADUAÇÃO UNIVERSITÁRIA // UNIVERSITY GRADUATE
-  // Status inicial da seção graduação
-  $("#sessao").hide();
-  $("#sessao1").hide();
-  $("#comentario").fadeIn();
-  $("#comentario1").show(); 
-  $("#btn-ocultar").hide();
-  $("#btn-ocultar1").hide();
-  $("#btn-ocultar-fim").hide();
-  $("#btn-ocultar-fim1").hide();
-  $("#btn-exibir").fadeIn();
-  $("#btn-exibir1").fadeIn();
-  // -----------------------------------------------------------
-  // Graduação em Eng Elétrica
+  // Graduação em Eng de Controle e Automação
   // Exibe texto escondido e oculta botão exibir
   // e exibe botões ocultar
   $("#btn-exibir").click(function(){
@@ -55,7 +125,7 @@ $(document).ready(function(){
       $("#btn-ocultar-fim").fadeIn();          
   });
   // -----------------------------------------------------------
-  // Graduação em Eng Elétrica
+  // Graduação em Eng de Controle e Automação
   // Esconde texto expandido e exibe o botão exibir
   // e oculta botões ocultar e faz scroll para o topo da seção
   // Hide displayed paragraphs e button
@@ -69,9 +139,11 @@ $(document).ready(function(){
     $("#comentario").fadeIn();
   });
   // -----------------------------------------------------------
-  // Scroll para graduação Eng Eletrica
+  // Scroll para Graduação em Eng de Controle e Automação
   // fecha textos expandidos da seção e faz scroll para o topo da seção
   $("#graduacaoeca").click(function() {
+    $(".group-index").fadeIn();
+    $(".group-blog").hide();
     $([document.documentElement, document.body]).animate({ scrollTop: $("#section11").offset().top -70 }, 1000);
     $("#sessao").hide();
     $("#sessao1").hide();
@@ -85,7 +157,7 @@ $(document).ready(function(){
     $("#btn-exibir1").fadeIn();
   });
   // -----------------------------------------------------------
-  // Graduação em Eng de Controle e Automação
+  // Graduação em Eng Elétrica
   // Esconde texto expandido e exibe o botão exibir
   // e oculta botões ocultar e faz scroll para o topo da seção
   $("#btn-ocultar1, #btn-ocultar-fim1").click(function()
@@ -98,7 +170,7 @@ $(document).ready(function(){
     $("#comentario1").fadeIn();
   });
   // -------------------------------------------------------------
-  // Graduação em Eng de Controle e Automação
+  // Graduação em Eng Elétrica
   // Exibe texto escondido e oculta botão exibir
   // e exibe botões ocultar
   $("#btn-exibir1").click(function()
@@ -110,10 +182,12 @@ $(document).ready(function(){
       $("#btn-ocultar-fim1").fadeIn(); 
   });
   // -------------------------------------------------------------
-  // Scroll para Eng de Controle e Automação
+  // Scroll para Graduação em Eng Elétrica
   // fecha textos expandidos da seção e faz scroll para o topo da seção
   $("#graduacaoee").click(function()
   {
+    $(".group-index").fadeIn();
+    $(".group-blog").hide();
     $([document.documentElement, document.body]).animate({ scrollTop: $("#section11").offset().top -70 }, 1000);
     $("#sessao").hide();
     $("#sessao1").hide();
